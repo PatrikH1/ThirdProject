c0f6626808db1be06df4f354d02a26f81eaeddf9
Jonas Hulthén <patrik.hulthen@live.com>
2023-01-29 02:56:40 +0100

    Added the new class BeraknaVinstSkatt and some updates in build.gradle.

--- src/main/java/vinstSkatt/BeraknaVinstSkatt.java
+++ src/main/java/vinstSkatt/BeraknaVinstSkatt.java
@@ -3,13 +3,21 @@ package vinstSkatt;
 import java.util.Scanner;
 
 public class BeraknaVinstSkatt {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int userInput = scanner.nextInt();
-        System.out.println("You entered: " + userInput);
+
+        System.out.print("Kapitaltillskott: ");
+        int kapitaltillskott = scanner.nextInt();
+
+        System.out.print("Inköpspris: ");
+        int inkopspris = scanner.nextInt();
+
+        System.out.println("Kapitaltillskott: " + kapitaltillskott);
+        System.out.println("Inköpspris: " + inkopspris);
+
         scanner.close();
     }
-
 
 }
