d5781c19cd8ce25a2427911ca1bd593581b87315
Jonas Hulthén <patrik.hulthen@live.com>
2024-01-13 01:21:25 +0100

    Updates with stream-tests.

--- src/main/java/Vector/VectorTest.java
+++ src/main/java/Vector/VectorTest.java
@@ -1,5 +1,6 @@
 package Vector;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Vector;
 
@@ -15,17 +16,58 @@ public class VectorTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Vector<InfoClass> vec = new Vector<InfoClass>();
-		
+		Vector<InfoClass> vec = new Vector<>();
+		ArrayList<InfoClass> array = new ArrayList<>();
+
+		// Init of vector
 		int max = 10;
-		for (int i = 0; i < max; i++) {
+		int i;
+		for (i = 0; i < max; i++) {
 			vec.add(new InfoClass("Info " + i));
 		}
-					
+
+		for (i = 0; i < vec.size(); i++) {
+			array.add(vec.get(i));
+		}
+
+		System.out.println("Info about Vector:");
 		Iterator<InfoClass> iter = vec.iterator();
 		while (iter.hasNext()) {
 			System.out.println(((InfoClass) iter.next()).getTextStr());
-		}		
+		}
+
+		System.out.println("\nInfo about array:");
+		array.stream().forEach(arr -> System.out.println(arr.getTextStr()));
+
+		String strToFind = "Info 9";
+
+		InfoClass result = vec
+				.stream()
+				.filter(res -> res.getTextStr().equals(strToFind))
+				.findFirst()
+				.orElse(null);
+
+		System.out.println();
+		if (result != null) {
+			System.out.println("(vec) Value found: " + strToFind);
+		}
+		else {
+			System.out.println("(vec) Value not found!");
+		}
+
+		result = array
+				.stream()
+				.filter(res -> res.getTextStr().equals(strToFind))
+				.findFirst()
+				.orElse(null);
+
+		if (result != null) {
+			System.out.println("(array) Value found: " + strToFind);
+		}
+		else {
+			System.out.println("(array) Value not found!");
+		}
+
 	}
 }
 
@@ -36,5 +78,5 @@ class InfoClass {
 	}
 	public String getTextStr() {
 		return textStr;
-	}	
+	}
 }
