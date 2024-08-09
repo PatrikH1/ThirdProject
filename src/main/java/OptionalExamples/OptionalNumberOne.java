60062d2fc3c0b3c0fcd9d4ff57d76c3573815b5c
PatrikHulthen <patrik.hulthen@live.com>
2024-08-09 23:58:11 +0200

    Some tests with Optional.

--- src/main/java/OptionalExamples/OptionalNumberOne.java
+++ src/main/java/OptionalExamples/OptionalNumberOne.java
@@ -1,10 +1,22 @@
 package OptionalExamples;
 
+import org.junit.Test;
+
 import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class OptionalNumberOne {
 
     public static void main(String[] args) {
+        // testOfOptional()
+        // testMethod1();
+
+        testMethod2();
+    }
+
+    public static void testOfOptional() {
         System.out.println("Simple program for testing Optional in Java:");
         System.out.println("--------------------------------------------");
         Optional obj1 = Optional.of
@@ -34,6 +46,22 @@ public class OptionalNumberOne {
         obj2.ifPresentOrElse(
                 s -> System.out.println("This is the content of obj2: " + s),
                 () -> System.out.println("No content of obj2!"));
+    }
+
+    @Test
+    public static void testMethod1() {
+        Optional<String> empty = Optional.empty();
+        assertFalse(empty.isPresent());
+    }
+
+    public static void testMethod2() {
+        Optional<Integer> aValue1 = Optional.of(35);
+        Optional<Integer> aValue2 = Optional.of(35);
+
+        System.out.println("Value of Optional Integer: " + aValue1);
+
+        System.out.println("Value of Optional Integer: " + (aValue1.get() + aValue2.get()));
+
     }
 
 }
