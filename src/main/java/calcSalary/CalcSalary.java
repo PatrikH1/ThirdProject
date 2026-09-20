78abffb7c547054b7083efeee45d0fe32e170b1d
PatrikHulthen <patrik.hulthen@live.com>
2026-09-20 21:19:29 +0200

    Uppdaterat CalcSalary.java och gradle-wrapper.properties

--- src/main/java/calcSalary/CalcSalary.java
+++ src/main/java/calcSalary/CalcSalary.java
@@ -19,18 +19,20 @@ public class CalcSalary {
 
         // Skriv ut skatt.
         System.out.println();
-        System.out.println("Moms: " + moms);
-        System.out.println("Bruttolon: " + bruttolon);
+        System.out.println("Moms: " + String.format("%.2f", moms));
+        System.out.println("Bruttolon: " + String.format("%.2f", bruttolon));
         String formateradArbetsgivaravgift = String.format("%.2f", arbetsgivaravgift);
         System.out.println("Arbetsgivaravgift: " + formateradArbetsgivaravgift);
         double skattSomBetalas = (bruttolon * ((double) skattesats / 100));
 
-        System.out.println("Skatt: " + skattSomBetalas);
+        // System.out.println("Skatt: " + skattSomBetalas);
+        System.out.println("Skatt: " + String.format("%.2f", (bruttolon * ((double) skattesats / 100))));
         double utbetaldLon = (double) lon - moms - skattSomBetalas - arbetsgivaravgift;
 
         System.out.println();
-        System.out.println("Utbetald lön: " + utbetaldLon);
-        System.out.println("Procentuell del som lön: " + ((utbetaldLon / (double) lon)) * 100);
+        System.out.println("Utbetald lön: " + String.format("%.2f", utbetaldLon));
+        System.out.println("Procentuell del som lön: " + String.format("%.2f", ((utbetaldLon / (double) lon)) * 100)
+                + "%");
 
         // Stäng scanner-objektet
         input.close();
